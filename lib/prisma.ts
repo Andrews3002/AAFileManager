05c5036659539b6888af3f6386e80dd16d02461f
Andrews3002 <alexangeloandrews@gmail.com>
2026-05-19 12:15:00 -0400

    packaging env with the distribution so the app can have access to the databaseurl in the .env file

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,6 +3,13 @@
 import { PrismaClient } from "../generated/prisma/index.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
+import dotenv from "dotenv";
+import path from "path";
+import { app } from "electron";
+
+dotenv.config({
+    path: app.isPackaged ? path.join(process.resourcesPath, ".env") : ".env",
+});
 
 const connectionString = process.env.DATABASE_URL!;
 
